770bac12692b6362fb3862453206de4156811e9d
Geloswift <angelo.nichele317@gmail.com>
2025-09-26 11:34:20 +0200

    complete the listingItem component

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -5,19 +5,39 @@ import { MdLocationOn } from "react-icons/md";
 
 export default function ListingItem({ listing, id }) {
   return (
-    <li>
-      <Link to={`/category/${listing.type}/${id}`}>
-        <img src={listing.imgUrls[0]} alt={listing.name} />
-        <Moment fromNow>
+    <li 
+      className="
+        relative bg-white flex flex-col justify-between items-center 
+        shadow-md hover:shadow-xl rounded-md overflow-hidden 
+        transition-shadow duration-150 m-2.5
+      "
+    >
+      <Link to={`/category/${listing.type}/${id}`} className="contents">
+        <img 
+          src={listing.imgUrls[0]} 
+          loading="lazy"
+          className="
+            h-44 w-full object-cover hover:scale-105
+            transition-scale duration-200 ease-in
+          "
+        />
+        <Moment 
+          fromNow
+          className="
+            absolute top-2 left-2 bg-[#3377cc] text-white 
+            uppercase text-xs font-semibold rounded-md 
+            px-2 py-1 shadow-lg
+          "
+        >
           {listing.timestamp?.toDate()}
         </Moment>
-        <div className="">
-          <div className="">
-            <MdLocationOn />
-            <p>{listing.address}</p>
+        <div className="w-full p-2.5">
+          <div className="flex items-center space-x-1">
+            <MdLocationOn className="h-4 w-4 text-green-600"/>
+            <p className="font-semibold text-sm mb-0.5 text-gray-600 truncate">{listing.address}</p>
           </div>
-          <p>{listing.name}</p>
-          <p>
+          <p className="font-semibold m-0 text-xl truncate">{listing.name}</p>
+          <p className="text-[#457b9d] mt-2 font-semibold">
             $
             {listing.offer
               ? listing.discountedPrice 
@@ -29,12 +49,12 @@ export default function ListingItem({ listing, id }) {
             }
             {listing.type === "rent" && " / month"}
           </p>
-          <div className="">
-            <div className="">
-              <p>{listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}</p>
+          <div className="flex items-center mt-2.5 space-x-3">
+            <div className="flex items-center space-x-1">
+              <p className="font-bold text-xs">{listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}</p>
             </div>
-            <div className="">
-              <p>{listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}</p>
+            <div className="flex items-center space-x-1">
+              <p className="font-bold text-xs">{listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}</p>
             </div>
           </div>
         </div>
